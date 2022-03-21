265f2de6956af634bfcc8aab745bc0c24012ede7
achebotarenko <90093648+achebotarenko@users.noreply.github.com>
2022-03-21 20:48:18 +0300

    ДЗ к уроку 1

--- task_1.py
+++ task_1.py
@@ -1,9 +1,13 @@
+# Посмотреть документацию к API GitHub,
+# разобраться как вывести список репозиториев для конкретного пользователя,
+# сохранить JSON-вывод в файле *.json.
+
 import requests
 import json
 from pprint import pprint
 
 
-#  username - achebotarenko
+# username - achebotarenko
 username = input('Enter username: ')
 response = requests.get(f'https://api.github.com/users/{username}/repos')
 
